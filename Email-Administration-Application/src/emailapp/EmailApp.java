6a6e2cb740662db771b62982c87dd2de8767adcd
Yusuf-Ali-1 <62290174+Yusuf-Ali-1@users.noreply.github.com>
2020-06-28 05:12:35 -0400

    FInished
    Email Admistration Applicaiton is done

---
+++ Email-Administration-Application/src/emailapp/EmailApp.java
@@ -0,0 +1,17 @@
+package emailapp;
+
+public class EmailApp {
+	
+	public static void main (String[] arg) {
+		
+		Email em1 = new Email ("John", "Smith" );
+		
+		System.out.println( em1.showInfo());
+		
+
+		
+		
+	
+
+	}
+}
